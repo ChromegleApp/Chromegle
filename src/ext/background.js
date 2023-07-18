aea8d44dc72c094c0bf39fd281a3e742b66d1784
isaac <isaacikogan@gmail.com>
2023-07-18 04:17:41 -0400

    Add onInstalled service worker, bump version

--- src/ext/background.js
+++ src/ext/background.js
@@ -1,10 +1,21 @@
 /**
  * Install Actions
  */
-chrome.runtime.onInstalled.addListener(function (){
+chrome.runtime.onInstalled.addListener(async function (){
+
+    // Check if it's been installed
+    let query = {HAS_BEEN_INSTALLED: "false"};
+    let hasBeenInstalled = (await chrome.storage.sync.get(query))["HAS_BEEN_INSTALLED"];
+    if (hasBeenInstalled === "true") {
+        return;
+    }
+
+    // Set to has been installed
+    query["HAS_BEEN_INSTALLED"] = "true";
+    await chrome.storage.sync.set(query);
 
     // Onboarding
-    chrome.tabs.create({url: "https://chromegle.net/"},function(){});
+    chrome.tabs.create({url: "https://chromegle.net/installed"},function(){});
 
     // Refresh Omegle
     chrome.tabs.query({url: 'https://www.omegle.com/*'}).then(tabs => {
